095088b9e9a4fcbf1daad18ed15e043602aabea8
Asher <Trisxtking@gmail.com>
2023-10-03 17:44:15 -0400

    Swerve config

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -46,21 +46,21 @@ public final class Constants {
         new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
 
     // Angular offsets of the modules relative to the chassis in radians
-    public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
+    public static final double kFrontLeftChassisAngularOffset = Math.PI / 2;
     public static final double kFrontRightChassisAngularOffset = 0;
     public static final double kBackLeftChassisAngularOffset = Math.PI;
-    public static final double kBackRightChassisAngularOffset = Math.PI / 2;
+    public static final double kBackRightChassisAngularOffset = -Math.PI / 2;
 
     // SPARK MAX CAN IDs
-    public static final int kFrontLeftDrivingCanId = 10;
-    public static final int kRearLeftDrivingCanId = 40;
-    public static final int kFrontRightDrivingCanId = 20;
-    public static final int kRearRightDrivingCanId = 30;
+    public static final int kFrontLeftDrivingCanId = 30;
+    public static final int kRearLeftDrivingCanId = 20;
+    public static final int kFrontRightDrivingCanId = 40;
+    public static final int kRearRightDrivingCanId = 10;
 
-    public static final int kFrontLeftTurningCanId = 11;
-    public static final int kRearLeftTurningCanId = 41;
-    public static final int kFrontRightTurningCanId = 21;
-    public static final int kRearRightTurningCanId = 31;
+    public static final int kFrontLeftTurningCanId = 31;
+    public static final int kRearLeftTurningCanId = 21;
+    public static final int kFrontRightTurningCanId = 41;
+    public static final int kRearRightTurningCanId = 11;
 
     public static final boolean kGyroReversed = false;
   }
